24f869e1fa57dd246cd6eb566b6357d6ba139c9d
Nickson Chen <540037@ibsh.tw>
2026-01-16 17:50:01 +0800

    updated main page

--- js/main.js
+++ js/main.js
@@ -24,3 +24,22 @@ if (glow) {
     glow.style.display = "none";
   });
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.unregister();
+      });
+    });
+  });
+}
+
+if ("caches" in window) {
+  window.addEventListener("load", () => {
+    caches.keys().then((keys) => {
+      keys.forEach((key) => {
+        caches.delete(key);
+      });
+    });
+  });
+}
